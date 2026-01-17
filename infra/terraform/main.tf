bc45fd298020b1f54227428fea33760145bd81cc
Your Name <you@example.com>
2026-01-17 16:50:32 +0100

    open grafana port

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port        = 3001
+    to_port          = 3001
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port        = 0
